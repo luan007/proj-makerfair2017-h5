50954aec55d4700eb71b0b629a800c3e27bc89ba
doge <luancfan@hotmail.com>
2017-08-02 16:33:57 +0800

    add stuff

---
+++ src/footer.js
@@ -0,0 +1,36 @@
+import "./styles/footer.less";
+
+export var config = {
+  autoHide: true,
+  visible: true,
+  threshold: 2,
+  selection: 0
+};
+
+var prevY = 0;
+
+function updateScroll() {
+  var realHeight =
+    document.scrollingElement.scrollHeight -
+    document.scrollingElement.clientHeight;
+  var curScroll = window.scrollY;
+  var dScroll = curScroll - prevY;
+  prevY = curScroll;
+
+  if (config.autoHide) {
+    config.visible =
+      curScroll < config.threshold || realHeight - curScroll < config.threshold; // ||
+    //   (dScroll < 0 && dScroll > -2);
+  } else {
+  }
+}
+
+$(document).ready(function() {
+  $("#footer > div").on("mousedown", function() {
+      config.selection = $(this).data('tab');
+  });
+});
+
+window.addEventListener("scroll", function(e) {
+  updateScroll();
+});
--- src/main.js
+++ src/main.js
@@ -1,2 +1,17 @@
-import "./styles/common.less"
-import "./styles/footer.less"
+import "./styles/common.less";
+import * as footer from "./footer.js";
+var data = {
+  ui: {
+    footer: footer.config
+  }
+};
+
+var computed = {};
+
+var app = new Vue({
+  el: "#app",
+  data: data,
+  computed: computed
+});
+
+window.data = data;
