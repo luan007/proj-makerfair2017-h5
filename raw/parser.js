89bf55ccb0981e88b19f958f667a2ceeb64d338e
doge <luancfan@hotmail.com>
2017-08-07 18:36:53 +0800

    for fack sake

--- raw/parser.js
+++ raw/parser.js
@@ -52,7 +52,6 @@ parseXlsx("keys.xlsx", function(err, data) {
     };
     spots[obj.key] = obj;
   }
-  console.log(spots);
   fs.writeFileSync(
     "keys.json",
     JSON.stringify({
@@ -60,4 +59,64 @@ parseXlsx("keys.xlsx", function(err, data) {
     }),
     "utf-8"
   );
+});
+
+parseXlsx("forum.xlsx", function(err, data) {
+  var all = [];
+  for (var i = 1; i < data.length; i++) {
+    var dt1 = new Date((parseInt(data[i][0]) - (25567 + 1)) * 86400 * 1000);
+    var dt2 = new Date((parseInt(data[i][0]) - (25567 + 1)) * 86400 * 1000);
+    var h1 = parseInt(data[i][1].split("-")[0].split(":")[0]);
+    var m1 = parseInt(data[i][1].split("-")[0].split(":")[1]);
+    var h2 = parseInt(data[i][1].split("-")[1].split(":")[0]);
+    var m2 = parseInt(data[i][1].split("-")[1].split(":")[1]);
+    dt1.setHours(h1, m1);
+    dt2.setHours(h2, m2);
+    var tag = data[i][2];
+    var title = data[i][3];
+    var name = data[i][5];
+    var job = data[i][6];
+    all.push({
+      id: all.length,
+      tag: tag,
+      title: title,
+      name: name,
+      job: job,
+      from: dt1,
+      to: dt2
+    });
+  }
+  fs.writeFileSync("forum.json", JSON.stringify(all), "utf-8");
+});
+
+parseXlsx("stage.xlsx", function(err, data) {
+  var all = [];
+  for (var i = 1; i < data.length; i++) {
+    var m = parseInt(data[i][0].trim().split(" ")[0]);
+    var d = parseInt(data[i][0].trim().split(" ")[1]);
+    var dt = new Date(0);
+    dt.setFullYear(2017, m - 1, d);
+    var dt1 = new Date(dt.getTime());
+    var dt2 = new Date(dt.getTime());
+    var h1 = parseInt(data[i][1].split("-")[0].split(":")[0]);
+    var m1 = parseInt(data[i][1].split("-")[0].split(":")[1]);
+    var h2 = parseInt(data[i][1].split("-")[1].split(":")[0]);
+    var m2 = parseInt(data[i][1].split("-")[1].split(":")[1]);
+    dt1.setHours(h1, m1);
+    dt2.setHours(h2, m2);
+    var title = data[i][2];
+    var desc = data[i][3];
+    var misc = data[i][4];
+    all.push({
+      title: title,
+      desc: desc,
+      misc: misc,
+      from: dt1,
+      to: dt2
+    });
+  }
+  all.sort((a, b) => {
+      return a.from > b.from;
+  });
+  fs.writeFileSync("stage.json", JSON.stringify(all), "utf-8");
 });
