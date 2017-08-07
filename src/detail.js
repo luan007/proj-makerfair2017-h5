9fea7a7cc76ade182643cc8f2c325e134f891004
doge <luancfan@hotmail.com>
2017-08-08 00:47:04 +0800

    good logic

--- src/detail.js
+++ src/detail.js
@@ -2,13 +2,15 @@ import "./styles/detail.less";
 import * as common from "./common.js";
 
 export var config = {
-    data: false
+    data: false,
+    src: false
 };
 
 common.events.on('rebind', function(ui) {
   config = ui.detail
 });
 
-common.methods.showDetail = function(e) {
+common.methods.showDetail = function(e, f) {
   config.data = e;
+  config.src = f;
 }
